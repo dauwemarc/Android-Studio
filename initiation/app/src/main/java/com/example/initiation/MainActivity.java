4d251687535dcf716e222014702146c7297c230f
dauwemarc <56584311+dauwemarc@users.noreply.github.com>
2020-05-18 11:53:45 +0200

    Add files via upload

--- initiation/app/src/main/java/com/example/initiation/MainActivity.java
+++ initiation/app/src/main/java/com/example/initiation/MainActivity.java
@@ -1,17 +1,32 @@
 package com.example.initiation;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+    class LinkButtons extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+        public void goToSo (View view) {
+            goToUrl ( "http://www.google.com/");
+        }
+
+        public void goToSu (View view) {
+            goToUrl ( "https://fr.yahoo.com/");
+        }
+
+        private void goToUrl (String url) {
+            Uri uriUrl = Uri.parse(url);
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+            startActivity(launchBrowser);
+        }
+
     }
-    public class public_void initiation(View view){
-        Intent browserIntent=new Intent(Intent.ACTION_VIEW,Uri.parse("http://www.google.com.kh"));
-        startActivity(browserIntent);
-}
+
